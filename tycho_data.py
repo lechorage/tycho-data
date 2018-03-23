00cbc360b122a4a6eae33af8557955cb87078bfa
Yuan Sun <yus74@pitt.edu>
2018-03-23 17:29:45 -0400

    ADD data

---
+++ tycho_data.py
@@ -0,0 +1,37 @@
+from requests import get
+import pandas as pd
+import os
+import csv
+import time
+
+
+def load_csv(file_name):
+    with open(file_name) as f:
+        return [{k: v for k, v in row.items()}
+                for row in csv.DictReader(f, skipinitialspace=True)]
+
+
+API_KEY = ""
+
+diseases = load_csv('diseases.csv')
+cities = load_csv('cities.csv')
+
+for disease in diseases:
+    for city in cities:
+        for event in ['cases', 'deaths']:
+            file_name = 'data/' + \
+                '_'.join([city['loc'], city['state'],
+                          disease['disease']]) + '.csv'
+            if os.path.exists(file_name):
+                continue
+            r = get('http://www.tycho.pitt.edu/api/query', params={'loc_type': 'city', 'loc': city['loc'], 'disease': disease['disease'],
+                                                                   'event': event, 'state': city['state'],
+                                                                   'start': 1888, 'end': 2014, 'apikey': API_KEY + '.csv'})
+
+            size = len(r.content)
+            if size <= 11 and 'No results' in r.text:
+                continue
+            print(r.url)
+            print(size, disease, city)
+            with open(file_name, 'wb') as f:
+                f.write(r.content)
